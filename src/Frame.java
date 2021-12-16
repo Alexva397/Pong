a72998ec2cdecb9f32459890d7292e8cc3db88fb
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-12-15 22:05:42 -0600

    created classes w/ constructors, outlined methods, working on displaying ui screen

---
+++ src/Frame.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Frame extends JFrame{
+	
+	GamePanel panel;
+	
+	Frame(){
+		panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("Pong");
+		this.setResizable(false);
+		this.setBackground(Color.black);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+}
---
+++ src/Gameplay.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Gameplay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Frame frame = new Frame();
+	}
+
+}
